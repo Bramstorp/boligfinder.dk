b243b638f12e47b3dfc051be12fe33f83d0d0567
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-04 12:51:34 +0200

    backend update

--- backend/base/agent/models.py
+++ backend/base/agent/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class AgentModel:
+class AgentModel(models.Model):
     full_name = models.CharField(max_length=50)
     photo = models.ImageField()
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=100)
     top_seller = models.BooleanField(default=False)
-    date_hired = models.DateTimeField(default=datetime.now, blank=True)
+    date_hired = models.DateTimeField(auto_now_add=True, blank=True)
 
-    def __str__(self):
-    	return self.full_name
+
+    
