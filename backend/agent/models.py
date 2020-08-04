b529742cc96ae3281c4ed1f2a416b15dca3b6af0
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-04 12:20:00 +0200

    update on agent models added str replace for object name

--- backend/agent/models.py
+++ backend/agent/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 class agentModel:
-    name = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=50)
     photo = models.ImageField()
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=100)
+    top_seller = models.BooleanField(default=False)
+    date_hired = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+    	return self.full_name
