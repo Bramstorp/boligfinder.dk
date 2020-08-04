1fdea97218ecc0e0dce0ecd32ff636ea1b548789
Nikolaj Ringgaard <niko7381@edu.aarhustech.dk>
2020-08-04 12:15:33 +0200

    agent model

--- backend/agent/models.py
+++ backend/agent/models.py
@@ -3,9 +3,7 @@ from django.db import models
 # Create your models here.
 class agentModel:
     name = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to='photos')
+    photo = models.ImageField()
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=100)
-    top_seller = models.BooleanField(default=False)
-    date_hired = models.DateTimeField(default=datetime.now, blank=True)
