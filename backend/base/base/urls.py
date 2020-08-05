c1c61b2460ecf98f3c01f341348668bb84cffd8d
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-05 21:10:57 +0200

    added / to url

--- backend/base/base/urls.py
+++ backend/base/base/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path("user/", include("user.urls")),
-    path("agent/api", include("agent.urls")),
-    path("contact/api", include("contact.urls")),
+    path("agent/api/", include("agent.urls")),
+    path("contact/api/", include("contact.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
