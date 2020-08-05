5146e6ea64554601f17a4e1eea5e3be583813145
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-05 13:45:51 +0200

    small syntax change to the listing model

--- backend/base/listing/models.py
+++ backend/base/listing/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class ListingModel(models.model)
-	SaleType = [
+	SALETYPE = [
 	    ('FOR_SALE', 'For Sale'),
 	    ('FOR_RENT', 'For Rent'),
 	]
-	HouseType = [
+	HOUSETYPE = [
 	    ('APARTMENT', 'Apartment'),
 	    ('VILLA', 'Villa'),
 	    ('TERRACED_HOUSE', 'Terraced house'),
@@ -17,11 +17,20 @@ class ListingModel(models.model)
 	slug = model.CharField(max_length=50),
 	title = model.CharField(max_length=50),
 	description = model.TextField(),
-	sale_type = model.CharField(choices=SaleType)
+	sale_type = model.CharField(choices=HOUSETYPE)
 	price = model.IntegerField(),
 	home_type = model.CharField(choices=HouseType),
 	open_house = models.BooleanField(default=False),
 	main_image = models.ImageField(),
-	image_1/20 = models.ImageField(),
-	is_published = models.BooleanField(),
-	listed_date = models.DateTimeField()
+	image_1 = models.ImageField(),
+	image_2 = models.ImageField(),
+	image_3 = models.ImageField(),
+	image_4 = models.ImageField(),
+	image_5 = models.ImageField(),
+	image_6 = models.ImageField(),
+	image_7 = models.ImageField(),
+	image_8 = models.ImageField(),
+	image_9 = models.ImageField(),
+	image_10 = models.ImageField(),
+	is_published = models.BooleanField(default=True),
+	listed_date = models.DateTimeField(auto_now_add=True)
