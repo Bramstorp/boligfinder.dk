2c073cdd9c3001fffedb570958785c7cefcce757
Nikolaj Ringgaard <niko7381@edu.aarhustech.dk>
2020-08-05 09:23:05 +0200

    House USer and stufff :)

--- backend/base/user/models.py
+++ backend/base/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class UserModel(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    adress = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
