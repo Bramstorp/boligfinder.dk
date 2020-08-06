cb5734c75c832aca929a0fc68d61488ad9a41e17
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-06 21:13:12 +0200

    i made better route system separatet routes and app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Layout from "./hocs/Layout"
+import {BrowserRouter as Router} from 'react-router-dom'
 
-import About from "./containers/About"
-import Contact from "./containers/Contact"
-import Homepage from "./containers/Homepage"
-import Listing_Details from "./containers/Listing_Details"
-import Listings from "./containers/Listings"
-import SignIn from "./containers/SignIn"
-import SignUp from "./containers/SignUp"
+import Layout from "./hocs/Layout"
+import BaseRoutes from "./routes"
 
 const App = () => (
-	<Router>
-		<Layout>
-			<Switch>
-				<Route exact path="/" component={Homepage} />
-				<Route exact path="/contact" component={Contact} />
-				<Route exact path="/listing_details/:id" component={Listing_Details} />
-				<Route exact path="/listings" component={Listings} />
-				<Route exact path="/about" component={About} />
-				<Route exact path="/signin" component={SignIn} />
-				<Route exact path="/signup" component={SignUp} />
-			</Switch>
-		</Layout>
-	</Router>
+	<div className="App">
+      	<Router>
+      		<Layout>
+	          <BaseRoutes />
+	        </Layout>
+      	</Router>
+    </div>
 )
 
 export default App;
---
+++ frontend/src/routes.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+import About from "./containers/About"
+import Contact from "./containers/Contact"
+import Homepage from "./containers/Homepage"
+import Listing_Details from "./containers/Listing_Details"
+import Listings from "./containers/Listings"
+import SignIn from "./containers/SignIn"
+import SignUp from "./containers/SignUp"
+
+const BaseRoutes  = () => (
+	<Router>
+		<Switch>
+			<Route exact path="/" component={Homepage} />
+			<Route exact path="/contact" component={Contact} />
+			<Route exact path="/listing_details/:id" component={Listing_Details} />
+			<Route exact path="/listings" component={Listings} />
+			<Route exact path="/about" component={About} />
+			<Route exact path="/signin" component={SignIn} />
+			<Route exact path="/signup" component={SignUp} />
+		</Switch>
+	</Router>
+)
+
+export default BaseRoutes;
