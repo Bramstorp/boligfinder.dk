5b771911f6e08e810c6af639403195dfd06fc649
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-06 09:29:23 +0200

    url order was change for better syntax

--- backend/base/base/urls.py
+++ backend/base/base/urls.py
@@ -24,9 +24,9 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path("user/", include("user.urls")),
-    path("agent/api/", include("agent.urls")),
-    path("contact/api/", include("contact.urls")),
+    path("api/user/", include("user.urls")),
+    path("api/agent/", include("agent.urls")),
+    path("api/contact/", include("contact.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
