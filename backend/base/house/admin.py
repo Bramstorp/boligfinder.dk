dc3364d6c9f5f5fcbb9ed351725fb3a77c917fe4
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-06 12:24:36 +0200

    Frontend updates setup work in progress

--- backend/base/house/admin.py
+++ backend/base/house/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import HouseModel
 
 # Register your models here.
-admin.site.Register(HouseModel)
+admin.site.register(HouseModel)
---
+++ frontend/src/containers/Listing_Details.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Listing_Details = () => (
+	<div>
+		Listing_Details
+	</div>
+)
+
+export default Listing_Details
