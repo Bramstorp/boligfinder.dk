068826083d291ca53c8ad5cb8a52a22c75f0cd37
nuttapong6565 <fluk0001@edu.aarhustech.dk>
2020-08-07 08:37:05 +0200

    Added Agent permission

--- backend/base/agent/views.py
+++ backend/base/agent/views.py
@@ -2,7 +2,9 @@ from rest_framework import viewsets
 
 from .models import AgentModel
 from .serializers import AgentSerializer
+from rest_framework.permissions import AllowAny
 
 class AgentViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     serializer_class = AgentSerializer
     queryset = AgentModel.objects.all()
--- backend/base/contact/views.py
+++ backend/base/contact/views.py
@@ -4,21 +4,7 @@ from .models import ContactModel
 from .serializers import ContactSerializer
 
 from rest_framework.permissions import AllowAny
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-
-# Create your views here.
 class ContactViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
     serializer_class = ContactSerializer
     queryset = ContactModel.objects.all()
-
-
-# class ContactPermission(APIView):
-#     permission_classes = [AllowAny]
-
-#     def get(self, request, format=None):
-#         content = {
-#             'status': 'request was permitted'
-#         }
-#         return Response(content)
