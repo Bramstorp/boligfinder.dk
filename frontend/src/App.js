53c08595be9a3b1b77d54901012c22ab9ce0b429
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-07 10:03:53 +0200

    made layout design not final but just so we can start testing api from backend to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
-
-import Layout from "./hocs/Layout"
 import BaseRoutes from "./routes"
+import 'antd/dist/antd.css';
 
-const App = () => (
-	<div className="App">
-      	<Router>
-      		<Layout>
-	          <BaseRoutes />
-	        </Layout>
-      	</Router>
-    </div>
-)
+import CustomLayout from "./hocs/Layout"
+
+class App extends Component {
+	render(){
+		return(
+			<div className="App">
+		      	<Router>
+		      		<CustomLayout>
+			          <BaseRoutes />
+			        </CustomLayout>
+		      	</Router>
+		    </div>
+	    )
+	}
+}
 
 export default App;
