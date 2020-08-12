6bce42fbdb5c3229f86168223505da7f3c938719
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-12 15:16:49 +0200

    FRONTEND UPDATE: listing form at lavet men ikke designet i nu bare lige en hurtigt boilerplate
    har ænder det layout grundet det ikke var speical pænt
    
    jeg vil lave / update navbar og en footer når jeg lige for tid

--- frontend/src/hocs/Layout.jsx
+++ frontend/src/hocs/Layout.jsx
@@ -1,32 +1,16 @@
 import React from "react"
-import { Layout, Menu, Breadcrumb } from 'antd';
 import { Link } from "react-router-dom"
-
-const { Header, Content, Footer } = Layout;
 
 class CustomLayout extends React.Component {
   render() {
     return (
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
-            <Breadcrumb.Item><Link to="/">List</Link></Breadcrumb.Item>
-          </Breadcrumb>
-            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-              {this.props.children}
-            </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-      </Layout> 
+      <div>
+        <h1>Navbar</h1>
+          <div>
+            {this.props.children}
+          </div>
+        <h1>Footer</h1>
+      </div> 
     )
   }
 }
