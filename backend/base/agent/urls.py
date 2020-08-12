16c6d1482acddfa9f78dfdc2187acb9bcbe844b8
niko7381 <niko.ringgaard@gmailcom>
2020-08-12 13:17:36 +0200

    MATHIAS der er FEJL!!!!! hjælp please!!!! KH. Niko

--- backend/base/agent/urls.py
+++ backend/base/agent/urls.py
@@ -1,6 +1,21 @@
-from .views import AgentViewSet
-from rest_framework.routers import DefaultRouter
+# from .views import AgentViewSet
+# from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r'', AgentViewSet, basename='agent')
-urlpatterns = router.urls
+# router = DefaultRouter()
+# router.register(r'', AgentViewSet, basename='agent')
+# urlpatterns = router.urls
+
+
+from django.urls import path
+
+from .views import (
+    AgentListCreateAPIView,
+    AgentUpdateView,
+    AgentDeleteView
+)
+
+urlpatterns = [
+    path("create/", AgentListCreateAPIView.as_view()),
+    path("<pk>/update", AgentUpdateView.as_view()),
+    path("<pk>/delete", AgentDeleteView.as_view())
+]
--- backend/base/contact/urls.py
+++ backend/base/contact/urls.py
@@ -1,6 +1,16 @@
-from .views import ContactViewSet
-from rest_framework.routers import DefaultRouter
+# from .views import ContactViewSet
+# from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r'', ContactViewSet, basename="contact")
-urlpatterns = router.urls
+# router = DefaultRouter()
+# router.register(r'', ContactViewSet, basename="contact")
+# urlpatterns = router.urls
+
+from django.urls import path
+
+from .views import (
+    ContactListCreateAPIView
+)
+
+urlpatterns = [
+    path("contact", ContactListCreateAPIView.as_view)
+]
