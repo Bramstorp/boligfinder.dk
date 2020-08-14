51eeac8089e5227ecf20e022d9790262d5380502
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-14 12:24:44 +0200

    small updates

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -2,6 +2,33 @@ import { setAlert } from './alert'
 import { SIGNUP_SUCCESS, SIGNUP_FAIL, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT} from './types'
 import axios from 'axios'
 
+export const signup = ({ name, email, password, password2 }) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify({ name, email, password, password2 })
+
+    try {
+        const res = await axios.post(`${process.env.REACT_API_URL}/api/accounts/signup`, body, config)
+
+        dispatch({
+            type: SIGNUP_SUCCESS,
+            payload: res.data
+        });
+
+        dispatch(login(email, password))
+    } catch (err) {
+        dispatch({
+            type: SIGNUP_FAIL
+        });
+
+        dispatch(setAlert('Error Authenticating', 'error'))
+    }
+}
+
 export const login = (email, password) => async dispatch => {
     const config = {
         headers: {
@@ -28,33 +55,6 @@ export const login = (email, password) => async dispatch => {
         dispatch(setAlert('Error Authenticating', 'error'))
     }
 }
-
-export const signup = ({ name, email, password, password2 }) => async dispatch => {
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-
-    const body = JSON.stringify({ name, email, password, password2 })
-
-    try {
-        const res = await axios.post(`${process.env.REACT_API_URL}/api/accounts/signup`, body, config)
-
-        dispatch({
-            type: SIGNUP_SUCCESS,
-            payload: res.data
-        });
-
-        dispatch(login(email, password))
-    } catch (err) {
-        dispatch({
-            type: SIGNUP_FAIL
-        });
-
-        dispatch(setAlert('Error Authenticating', 'error'))
-    }
-};
 
 export const logout = () => dispatch => {
     dispatch(setAlert('logout successful.', 'success'))
