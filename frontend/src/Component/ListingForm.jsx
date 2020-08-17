108fa558f4beed678ff169a9a061b334d083c4f4
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-17 19:15:52 +0200

    download fontawesome to the project :) and made basic listing form :D

--- frontend/src/Component/ListingForm.jsx
+++ frontend/src/Component/ListingForm.jsx
@@ -1,12 +1,86 @@
 import React from 'react'
-import { Select } from 'antd';
-import { Row, Col } from 'antd';
 import "../assets/ListingForm.style.scss"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ListingForm = () => (
-	<div>
-		Listform
-    </div>
+<div className="container">
+
+	<form>
+
+		{/* Row 1*/}
+		<div className="row">
+
+			<div className="col">
+		    <label for="">Sale or Rent</label>
+		    <select class="form-control">
+		      <option>1</option>
+		    </select>
+		  </div>
+
+		  <div className="col">
+		    <label for="">Minimum Price</label>
+		    <select class="form-control">
+		      <option>1</option>
+		    </select>
+		  </div>
+
+		  <div className="col">
+		    <label for="">Maximum Price</label>
+		    <select class="form-control">
+		      <option>1</option>
+		    </select>
+		  </div>
+
+		  <div className="col">
+		    <label for="">House Type</label>
+		    <select class="form-control">
+		      <option>1</option>
+		    </select>
+		  </div>		      	  
+	  </div>
+
+	  <br />
+
+		{/* Row 2*/}
+	  <div className="row">
+
+			<div className="col">
+		    <label for="">Sqft</label>
+		    <select class="form-control">
+		      <option>1</option>
+		    </select>
+		  </div>
+
+		  <div className="col">
+		    <label for="">Bathrooms</label>
+		    <select class="form-control">
+		      <option>1</option>
+		    </select>
+		  </div>
+
+		  <div className="col">
+		    <label for="">Bedrooms</label>
+		    <select class="form-control">
+		      <option>1</option>
+		    </select>
+		  </div>
+
+		  <div className="col">
+		    <label for="">Days Listed</label>
+		    <select class="form-control">
+		      <option>1</option>
+		    </select>
+		  </div>		      	  
+	  </div>
+	</form>
+
+	<br />
+  <input class="form-control" type="search" placeholder="Search" aria-label="Search"/>
+  <br />
+  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+
+
+</div>
 )
 
 export default ListingForm
