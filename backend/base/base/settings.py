417a1afff1e31c53be2fca7a261417101f1aad62
Bramstorp <m.bramstorp@stofanet.dk>
2020-08-24 12:41:07 +0200

    fixed base media

--- backend/base/base/settings.py
+++ backend/base/base/settings.py
@@ -126,14 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-MEDIA_URL = "/images/"
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, 'build/static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # REST_FRAMEWORK
 REST_FRAMEWORK = {
