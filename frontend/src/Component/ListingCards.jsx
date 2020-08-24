d51f740f2b2c8b577d992d7f5e8058cb5d9b30dd
nuttapong6565 <fluk0001@edu.aarhustech.dk>
2020-08-24 14:12:54 +0200

    Added details for each house.

--- frontend/src/Component/ListingCards.jsx
+++ frontend/src/Component/ListingCards.jsx
@@ -5,20 +5,29 @@ import { BrowserRouter as Router, Link } from 'react-router-dom';
 class ListingCard extends React.Component {
 	render() {
 		
-		const card = this.props.data.map(item => {
+		const card = this.props.data.map((item, index) => {
 			return (
 			<div className="card">
 			  <img className="card-img-top" src="https://via.placeholder.com/300x200"/>
 			  <div className="card-body">
 			    <h5 className="card-title">{item.title}</h5>
 			    <p className="card-text">{item.description}</p>
-			    <Link to={`/listing_details/${item.slug}`} className="btn btn-primary" >View House</Link>
+				<Link to=
+				// {`/listing_details/${item.slug}`}
+				{ 
+					{
+						pathname: "/listing_details/" + item.slug,
+						myHouse: this.props.data[index]
+					}
+				}
+				className="btn btn-primary" >View House</Link>
 			    <p className="">{item.price}</p>
 			    <p className="">{item.sale_type}</p>
 			  </div>
 			</div>
 			)
 		})
+		
 
 		return (
 			<div className="row">
--- frontend/src/containers/Listing_Details.jsx
+++ frontend/src/containers/Listing_Details.jsx
@@ -2,11 +2,14 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 import ListingDetailsView from "../Component/ListingDetails"
 
-const ListingDetails = (props) => {
-	return (
-		<div>test</div>
-	)
+class ListingDetails extends React.Component  {
+	render() {		
+		return (
+			<div className="container">
+				<ListingDetailsView data={this.props.location.myHouse}/>				
+			</div>
+		)
+	}
 }
+
 export default ListingDetails
-
-
