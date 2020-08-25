eaf553500c308bba587f82a5f6908e6999b4d0e6
nuttapong6565 <fluk0001@edu.aarhustech.dk>
2020-08-25 10:58:53 +0200

    Fix listing_details from home page

--- frontend/src/Component/ListingDetails.jsx
+++ frontend/src/Component/ListingDetails.jsx
@@ -1,60 +1,73 @@
 import React from 'react'
 import { Row, Col, Descriptions  } from 'antd';
 import { Empty } from 'antd';
+import axios from 'axios'
 
 class ListingDetailsView extends React.Component {
-	render() {
-		const contentStyle = {
-			height: '500px',
-			color: '#fff',
-			lineHeight: '500px',
-			textAlign: 'center',
-			background: '#364d79',
-		};
-
-		if (this.props.data == null)
-			window.location = "/NotFound"
+	state = {
+		listings: []
+	}
+	componentDidMount() {
+		if (this.props.data == null) {
+			axios.get("http://127.0.0.1:8000/api/listing/")
+			.then(res => {
+				const item = window.location.href.substring(window.location.href.lastIndexOf('/') + 1)
+				var index = res.data.findIndex(function(person) {
+					return person.slug == item
+				});
+				this.setState({
+					listings: res.data[index]
+				})
+				//console.log(res.data[index])
+			})	
+		}
 		else {
-			console.log(this.props.data)
-			var house = this.props.data;
-			var house_d = house.house;
-			if (house_d)
-				return (
-					<div>
-						<Row>
-							<Col span={24}>
-								<Descriptions
-									title={house.title}
-									bordered
-									column={{ xxl: 1, xl: 1, lg: 1, md: 1, sm: 1, xs: 1 }}
-								>
-									<Descriptions.Item label="Home type">{house_d.home_type}</Descriptions.Item>
-									<Descriptions.Item label="Area">{house_d.area}</Descriptions.Item>
-									<Descriptions.Item label="Ground area">{house_d.area_ground}</Descriptions.Item>
-									<Descriptions.Item label="Bathrooms">{house_d.bathrooms}</Descriptions.Item>
-									<Descriptions.Item label="Bedrooms">{house_d.bedrooms}</Descriptions.Item>
-									<Descriptions.Item label="Rooms">{house_d.rooms}</Descriptions.Item>
-									<Descriptions.Item label="Floor">{house_d.floors}</Descriptions.Item>
-									<Descriptions.Item></Descriptions.Item>
-									<Descriptions.Item label="Address">{house_d.address}</Descriptions.Item>
-									<Descriptions.Item label="City">{house_d.city}</Descriptions.Item>
-									<Descriptions.Item label="State">{house_d.state}</Descriptions.Item>
-									<Descriptions.Item label="Zip code">{house_d.zipcode}</Descriptions.Item>
-									<Descriptions.Item></Descriptions.Item>
-									<Descriptions.Item label="Images">
-										<img
-										src={house.main_image ? house.main_image : "https://upload.wikimedia.org/wikipedia/commons/6/6c/No_image_3x4.svg"}
-										/>
-									</Descriptions.Item>
-								</Descriptions>
-							</Col>
-						</Row>
-					</div>
-				)
-			else 
-				return <div><Empty /></div>
+			// console.log(this.props.data)
+			this.setState({
+				listings: this.props.data
+			})
 		}
 	}
+
+	render() {
+		var house = this.state.listings;
+		var house_d = house.house;
+		if (house_d)
+			return (
+				<div>
+					<Row>
+						<Col span={24}>
+							<Descriptions
+								title={house.title}
+								bordered
+								column={{ xxl: 1, xl: 1, lg: 1, md: 1, sm: 1, xs: 1 }}
+							>
+								<Descriptions.Item label="Home type">{house_d.home_type}</Descriptions.Item>
+								<Descriptions.Item label="Area">{house_d.area}</Descriptions.Item>
+								<Descriptions.Item label="Ground area">{house_d.area_ground}</Descriptions.Item>
+								<Descriptions.Item label="Bathrooms">{house_d.bathrooms}</Descriptions.Item>
+								<Descriptions.Item label="Bedrooms">{house_d.bedrooms}</Descriptions.Item>
+								<Descriptions.Item label="Rooms">{house_d.rooms}</Descriptions.Item>
+								<Descriptions.Item label="Floor">{house_d.floors}</Descriptions.Item>
+								<Descriptions.Item></Descriptions.Item>
+								<Descriptions.Item label="Address">{house_d.address}</Descriptions.Item>
+								<Descriptions.Item label="City">{house_d.city}</Descriptions.Item>
+								<Descriptions.Item label="State">{house_d.state}</Descriptions.Item>
+								<Descriptions.Item label="Zip code">{house_d.zipcode}</Descriptions.Item>
+								<Descriptions.Item></Descriptions.Item>
+								<Descriptions.Item label="Images">
+									<img
+									src={house.main_image ? house.main_image : "https://upload.wikimedia.org/wikipedia/commons/6/6c/No_image_3x4.svg"}
+									/>
+								</Descriptions.Item>
+							</Descriptions>
+						</Col>
+					</Row>
+				</div>
+			)
+		else 
+			return <div><Empty /></div>
+		}
 }
 
 export default ListingDetailsView
